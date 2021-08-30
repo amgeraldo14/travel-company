ac25c03acfc40fb4d84c6a340fa790ee37dcb804
geraldo <amgeraldo14@gmail.com>
2021-08-31 01:46:12 +0700

    header done, map on progress

---
+++ src/components/List/List.jsx
@@ -0,0 +1,11 @@
+import { CircularProgress, Grid, Typography, InputLabel, MenuItem, FormControl, Select } from "@material-ui/core"
+import useStyles from './styles'
+
+const List = () => {
+  const classes = useStyles()
+  return (
+    <h1> List </h1>
+  )
+}
+
+export default List
